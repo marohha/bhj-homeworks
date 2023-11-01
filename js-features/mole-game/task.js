372d081ef00e92294fbc957ada4b81b7883bee02
Marohha <marohha@yandex.ru>
2023-11-01 10:47:35 +0300

    Homework is done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,11 +1,9 @@
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
-getHole = index => document.getElementById(`hole${index}`);
-const hole1 = document.getElementById("hole1");
+const hole = Array.from(document.querySelectorAll(".hole"));
 
-for (i = 1; i = 10; i++) {
-    // index[i].onclick
-    getHole[i].onclick = () => {
+hole.forEach(item => {
+    item.onclick = () => {
         if (dead.textContent === "10") {
             alert("Вы победили! )))");
             dead.textContent = 0;
@@ -16,34 +14,7 @@ for (i = 1; i = 10; i++) {
             dead.textContent = 0;
             lost.textContent = 0;
         } 
-        if (getHole(index).classList.contains("hole_has-mole")) {
-            dead.textContent++;   
-        } else {
-            lost.textContent++;     
-        }
+
+        item.classList.contains("hole_has-mole") ? dead.textContent++ : lost.textContent++
     }
-}
-
-
-
-// for (Index = 1; Index = 10; index++) {
-//     let hole = getHole(index);
-//     hole.onclick = () => {
-//         if (hole.classList.contains("hole_has-mole")) {
-//             dead.textContent++;   
-//         } else {
-//             lost.textContent++;     
-//         }
-
-//         if (dead.textContent === 10) {
-//             alert("Вы победили! )))");
-//             dead.textContent = 0;
-//             lost.textContent = 0;
-//         }
-//         if (lost.textContent === 5) {
-//             alert("Вы проиграли (((");
-//             dead.textContent = 0;
-//             lost.textContent = 0;
-//         }
-//     }
-// }
+})
