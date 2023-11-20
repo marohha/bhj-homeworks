fac6221f98815cb323b0bac29876c2a17dc20c03
Marohha <marohha@yandex.ru>
2023-11-20 21:00:18 +0300

    HW

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,24 +1,38 @@
 const rotator = Array.from(document.querySelectorAll('.rotator__case'));
 
-rotator.forEach((item) => {
-    item.classList.remove('rotator__case_active');
-    item.nextElementSibling.classList.add('rotator__case_active');
-})
+let currentIndex = rotator.findIndex(item => item.classList.contains('rotator__case_active'));
 
-// const intervaId = setInterval(() => {
-//     rotator.forEach((item) => {
-//         item.classList.remove('rotator__case_active');
-//         item.nextElementSibling.classList.toggle('rotator__case_active');
-//     })
-   
-//     // rotator.forEach((item) => {
-//     //     // rotator.forEach((item) => item.classList.remove('rotator__case_active'));
-//     //     item.classList.remove('rotator__case_active');
-//     //     (item+1).classList.add('rotator__case_active');
-//     // })
+const getNextIndex = () => currentIndex === rotator.length - 1 ? 0 : currentIndex + 1;
 
-//     // for (let i = 0; i < rotator.length; i++) {
-//     //     rotator[i].classList.remove('rotator__case_active');
-//     //     rotator.nextElementSibling.classList.add('rotator__case_active');
-//     // }
-// }, 1000)
+const toggleNextCase = () => {
+    const currentCase = rotator[currentIndex];
+    
+    const nextIndex = getNextIndex();
+    const nextCase = rotator[nextIndex];
+    
+    currentCase.classList.remove('rotator__case_active');
+    nextCase.classList.add('rotator__case_active');
+    nextCase.style.color = nextCase.dataset.color;
+    
+    currentIndex = nextIndex;
+    
+    scheduleCase(nextIndex);
+  }
+  
+  const scheduleCase = index => {
+    const item = rotator[index];
+    
+    const speed = +item.dataset.speed;
+    setTimeout(toggleNextCase, speed);
+  }
+  
+  scheduleCase(getNextIndex());
+
+
+// rotator.forEach((item) => {
+//     item.classList.remove('rotator__case_active');
+//     if (nextElementSibling === null) {
+
+//     }
+//     item.nextElementSibling.classList.add('rotator__case_active');
+// })
