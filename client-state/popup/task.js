7068f9954aeae65168452ee27bd1eee7132b6448
Marohha <marohha@yandex.ru>
2023-11-29 17:42:59 +0300

    HW

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,29 +1,7 @@
 const popup = document.getElementById('subscribe-modal');
 const closeBtn = document.querySelector('.modal__close');
 
-if (localStorage.getItem('visit') === '1') {
-    popup.classList.remove('modal_active');
-} else {
-    document.addEventListener('DOMContentLoaded', () => {
-        popup.classList.add('modal_active');
-    })
-    closeBtn.addEventListener('click', () => {
-        popup.classList.remove('modal_active');
-        localStorage.setItem('visit', 1);
-    })
-    
-}
-// const setCookie = (name, value) => {
-//     document.cookie = name + '=' + encodeURIComponent(value);
-// }
-
-// const getCookie = (name) => {
-//     const pairs = document.cookie.split('; ');
-//     const cookie = pairs.find(p => p.startsWith(name + '='));
-//     return cookie.substr(name.length + 1);
-// }
-
-// if (document.cookie === 'visit=1') {
+// if (localStorage.getItem('visit') === '1') {
 //     popup.classList.remove('modal_active');
 // } else {
 //     document.addEventListener('DOMContentLoaded', () => {
@@ -31,6 +9,40 @@ if (localStorage.getItem('visit') === '1') {
 //     })
 //     closeBtn.addEventListener('click', () => {
 //         popup.classList.remove('modal_active');
-//         document.cookie = 'visit=1';
-//     })   
-// }
+//         localStorage.setItem('visit', 1);
+//     })
+    
+// } 
+// Выше - это решение через сохранение в localStorage. Оно работает. 
+
+
+// Ниже - это решение мое через cookie. Не могу понять что делаю не так. 
+// Не записываются и не выводит даже в консоль куки, выводит пустую строку.
+
+const setCookie = (name, value) => {
+    document.cookie = name + '=' + encodeURIComponent(value);
+}
+
+const getCookie = (name) => {
+    const pairs = document.cookie.split('; ');
+    const cookie = pairs.find(p => p.startsWith(name + '='));
+    return cookie.substr(name.length + 1);
+}
+
+setCookie('visit', '1');
+
+console.log(getCookie('visit')); 
+// при таком выводе пишет ошибку, TypeError: Cannot read properties of undefined (reading 'substr')
+
+if (document.cookie === 'visit=1') {
+    popup.classList.remove('modal_active');
+} else {
+    document.addEventListener('DOMContentLoaded', () => {
+        popup.classList.add('modal_active');
+    })
+    closeBtn.addEventListener('click', () => {
+        popup.classList.remove('modal_active');
+        document.cookie = 'visit=1';
+        console.log(document.cookie);
+    })   
+}
